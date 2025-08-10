bb45271e8eca7de9c5d54576445c276c7f46fda5
harcao <harvey.cao.yy@gmail.com>
2025-08-10 16:53:51 +1200

    Jinja, Macros, and Packages

---
+++ dbt_refactoring_sql_for_modularity/models/marts/fct_orders.sql
@@ -0,0 +1,41 @@
+{{
+    config(
+        materialized='incremental'
+    )
+}}
+with orders as  (
+    select * from {{ ref ('stg_jaffle_shop__orders' )}}
+),
+
+payments as (
+    select * from {{ ref ('stg_stripe__payments') }}
+),
+
+order_payments as (
+    select
+        order_id,
+        sum (case when payment_status = 'success' then payment_amount end) as amount
+
+    from payments
+    group by 1
+),
+
+ final as (
+
+    select
+        orders.order_id,
+        orders.customer_id,
+        orders.order_placed_at as order_date,
+        coalesce (order_payments.amount, 0) as amount
+
+    from orders
+    left join order_payments using (order_id)
+)
+
+select * from final
+
+
+{% if is_incremental() %}
+where
+order_date >= (select max(order_date) from {{this}})
+{% endif %}
